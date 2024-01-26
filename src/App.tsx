4ed272225e9e7ba00dff99865350ccf25bbe7a41
DeepralShakya <shakyadeepral44@gmail.com>
2024-01-26 12:38:29 +0545

    drawing bar

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import './App.css';
+import wordsData from "./wordlist.json";
+import { HangmanDrawing } from "./components/HangmanDrawing";
+import { HangmanWord } from "./components/HangmanWord";
+import { Keyboard } from "./components/Keyboard";
+
+type WordsData = {
+  words: string[];
+};
+
+function App() {
+  const words: WordsData = wordsData;
+
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    const randomIndex = Math.floor(Math.random() * words.words.length);
+    return words.words[randomIndex];
+  });
+  const [guessedLetter, setGuessLetter] = useState<string[]>([])
+
+
+
+  return(
+  <div className="container">
+    <div className="header">Hangman</div>
+      <div className="#">
+        <HangmanDrawing/>
+        <HangmanWord/>
+        <Keyboard/>
+      </div>
+  </div>
+  )
+}
+
+export default App;
---
+++ src/components/HangmanDrawing.tsx
@@ -0,0 +1,13 @@
+import "../App.css"
+
+export function HangmanDrawing(){
+    return (
+    <div className="drawing">
+        <div className="flatbar">
+            <div className="straightline">
+            
+            </div>
+        </div>
+    </div>
+    )
+}
